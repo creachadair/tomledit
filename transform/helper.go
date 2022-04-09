3a2691042143c6cd786f0e7be536c6190addc4c6
M. J. Fromberger <michael.j.fromberger@gmail.com>
2022-04-09 13:51:26 -0700

    Add SortKeyValuesByName helper.
    Permute a slice of items to reorder the KeyValue items by their names, but
    leaving all other elements of the slice in their original positions.

--- transform/helper.go
+++ transform/helper.go
@@ -35,3 +35,39 @@ func nameOf(s *tomledit.Section) parser.Key {
 	}
 	return s.Heading.Name
 }
+
+// SortKeyValuesByName performs a stable in-place sort of items, so that any
+// key-value entries are ordered by their names, but other items such as
+// comments are left in their original positions.
+func SortKeyValuesByName(items []parser.Item) {
+	s := subseq{orig: items}
+	for i, item := range items {
+		kv, ok := item.(*parser.KeyValue)
+		if ok {
+			s.pos = append(s.pos, i)
+			s.name = append(s.name, kv.Name)
+		}
+	}
+
+	sort.Stable(s)
+}
+
+// subseq implements sort.Interface to sort a subsequence of the elements of
+// the original slice. It builds mirror slices of the names and indices of the
+// items to be sorted in the original slice, then sorts these. When swapping
+// elements of the mirror slices, the corresponding elements of the original
+// are also swapped.
+type subseq struct {
+	orig []parser.Item // the original input slice
+	pos  []int         // offset in orig of the ith subsequence item
+	name []parser.Key  // the key of the current ith subsequence item
+}
+
+func (s subseq) Len() int           { return len(s.pos) }
+func (s subseq) Less(i, j int) bool { return s.name[i].Before(s.name[j]) }
+
+func (s subseq) Swap(i, j int) {
+	oi, oj := s.pos[i], s.pos[j]
+	s.orig[oi], s.orig[oj] = s.orig[oj], s.orig[oi]
+	s.name[i], s.name[j] = s.name[j], s.name[i]
+}
