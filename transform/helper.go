69dc50347a95adf0469942b872305db9e5e2e770
M. J. Fromberger <michael.j.fromberger@gmail.com>
2022-04-09 08:24:54 -0700

    Add SortSectionsByName helper.

--- transform/helper.go
+++ transform/helper.go
@@ -3,6 +3,8 @@
 package transform
 
 import (
+	"sort"
+
 	"github.com/creachadair/tomledit"
 	"github.com/creachadair/tomledit/parser"
 )
@@ -17,4 +19,31 @@ func FindTable(doc *tomledit.Document, name ...string) *tomledit.Entry {
 		}
 	}
 	return nil
+}
+
+// SortSectionsByName performs a stable in-place sort of the given slice of
+// sections by their name.
+func SortSectionsByName(ss []*tomledit.Section) {
+	sort.SliceStable(ss, func(i, j int) bool {
+		return keyLess(nameOf(ss[i]), nameOf(ss[j]))
+	})
+}
+
+func nameOf(s *tomledit.Section) parser.Key {
+	if s.Heading == nil {
+		return nil
+	}
+	return s.Heading.Name
+}
+
+func keyLess(k1, k2 parser.Key) bool {
+	i, j := 0, 0
+	for i < len(k1) && j < len(k2) {
+		if k1[i] < k2[j] {
+			return true
+		}
+		i++
+		j++
+	}
+	return i == len(k1) && j < len(k2)
 }
